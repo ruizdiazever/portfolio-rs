0f96d3fc43f5cd79cbd5d0319a4d955f7e8558b4
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2024-06-17 22:53:58 +0200

    fix: deleted unused files form lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,11 @@ pub mod layouts {
     pub mod layout;
 
     pub mod components {
-        pub mod footer;
         pub mod header;
     }
 }
 
 pub mod components {
-    pub mod common {
-        pub mod button;
-        pub mod icons;
-        pub mod link;
-    }
     pub mod home {
         pub mod experience;
         pub mod project;
