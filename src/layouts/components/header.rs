af9a98fbaf056649d2cdf54aa586a1b00251f1af
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2024-06-23 19:23:37 +0200

    chore: minor improvement

--- src/layouts/components/header.rs
+++ src/layouts/components/header.rs
@@ -19,7 +19,7 @@ pub fn Header() -> impl IntoView {
                 bg-muted focus:outline-none focus:ring-1 focus:ring-ring"
                 size="icon"
             >
-              <Icon class="h-5 w-5" icon=i::AiSearchOutlined />
+              <Icon class="h-5 w-5" icon=i::CgSearch />
               <span class="sr-only">Search</span>
             </button>
             <Icon class="h-5 w-5 transform rotate-90 text-gray-500" icon=i::AiMinusOutlined />
@@ -40,15 +40,6 @@ pub fn Header() -> impl IntoView {
             >
               <Icon class="h-5 w-5" icon=i::FaGithubBrands />
               <span class="sr-only">GitHub</span>
-            </a>
-            <a
-                href="https://www.linkedin.com/" rel="noopener"  target="_blank"
-                class="inline-flex items-center justify-center text-gray-700 hover:text-[#68b5fc]
-                w-8 h-8 rounded-md text-sm font-medium transition hover:bg-gray-100
-                bg-muted focus:outline-none focus:ring-1 focus:ring-ring duration-200"
-            >
-              <Icon class="h-5 w-5" icon=i::BsTwitterX />
-              <span class="sr-only">Twitter</span>
             </a>
           </div>
         </header>
