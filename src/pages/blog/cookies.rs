3f20848c3e9c1ae1c16e5227c80cddeffd9e56bb
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2024-09-02 18:20:45 +0200

    chore: minor change

--- src/pages/blog/cookies.rs
+++ src/pages/blog/cookies.rs
@@ -6,8 +6,8 @@ use leptos::*;
 pub fn Cookies() -> impl IntoView {
     // Project
     let id = "f7583be4-ebf7-48a9-928d-5a058f0aabd9".to_string();
-    let title = "Refresh/access token with Rust".to_string();
-    let subtitle = "Auth security with Axum, GraphQL and Rust".to_string();
+    let title = "Secure auth with Rust".to_string();
+    let subtitle = "Refresh/access token with Axum and async-graphql".to_string();
     let date = "Aug 31, 2024".to_string();
     let tags_key = "cookies";
 
