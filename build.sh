be9b8251d74eb4902f0d55f2d97d744ab460451e
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2024-12-16 21:10:29 +0100

    chore: minor change

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 echo "Building PostgreSQL, RedisDB and WASM of Portfolio WASM..."
 sudo docker compose build --no-cache && sudo docker compose up -d
 
-echo "PostgreSQL and RedisDB started and running in the background."
+echo "Portfolio WASM started and running in the background."
