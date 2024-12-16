7c2576636a5a8dcaba65bc81339e9e68102e9dae
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2024-12-16 22:39:45 +0100

    chore: minor

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the services
-echo "Building PostgreSQL, RedisDB and WASM of Portfolio WASM..."
+echo "Building Portfolio WASM..."
 sudo docker compose build --no-cache && sudo docker compose up -d
 
 echo "Portfolio WASM started and running in the background."
