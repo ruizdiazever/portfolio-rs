e0ba99820f1ea46b5a248dda95f27d4d7db6b160
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2025-01-07 14:15:33 +0100

    chore: minor

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ export LEPTOS_SITE_ADDR=0.0.0.0:3003
 export LEPTOS_RELOAD_PORT=3020
 export API_URL=http://localhost:3002
 
-if [ -f .env ]; then
-    export $(cat .env | xargs)
-fi
+# if [ -f .env ]; then
+#     export $(cat .env | xargs)
+# fi
 
 npx tailwindcss -i ./style/tailwind.css -o ./style/output.css
 cargo leptos watch
