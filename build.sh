52d9dcbf5182df523ac34a0463405107dde01efc
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2025-02-24 21:19:02 +0100

    chore: improvement

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # Build the services
 echo "Building Portfolio..."
-sudo docker compose build --project-name portfolio --no-cache && sudo docker compose --project-name portfolio up -d --force-recreate
+sudo docker compose build -p portfolio --no-cache && sudo docker compose -p portfolio up -d --force-recreate
 echo "Portfolio started and running in the background."
