ca7e8b273454fe5ba83c783a3237befe16dafd1a
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2025-02-28 22:33:18 +0100

    feat: deploy

---
+++ deploy_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the services
+echo "Building Portfolio..."
+sudo docker compose -p portfolio build --no-cache frontend
+sudo docker compose -p portfolio up --force-recreate -d frontend
+echo "Portfolio started and running in the background."
