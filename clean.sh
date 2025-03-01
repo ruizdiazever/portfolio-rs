79b894d596382a868dee622bbdbda630e69aeac2
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2025-03-01 10:21:48 +0100

    fix: sudo

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker volume ls -q | grep -v "portfolio_redis_data" | xargs -r docker volume rm
-docker system prune -a
+sudo docker volume ls -q | grep -v "portfolio_redis_data" | xargs -r docker volume rm
+sudo docker system prune -a
